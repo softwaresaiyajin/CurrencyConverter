bb98a465871f2864f936c24888cfc29989ab6414
software.saiyajin <gerald.adorza@navitaire.com>
2019-12-18 23:54:43 +0800

    Added dependencies

---
+++ Modules/HomeModule/Demo/Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Gerald on 18/12/2019.
+//  Copyright © 2019 Gerald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Modules/HomeModule/HomeModule/HomeModule.h
@@ -0,0 +1,19 @@
+//
+//  HomeModule.h
+//  HomeModule
+//
+//  Created by Gerald on 18/12/2019.
+//  Copyright © 2019 Gerald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HomeModule.
+FOUNDATION_EXPORT double HomeModuleVersionNumber;
+
+//! Project version string for HomeModule.
+FOUNDATION_EXPORT const unsigned char HomeModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomeModule/PublicHeader.h>
+
+
